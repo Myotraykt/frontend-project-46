5099f33d58189c7970dacee205995b23d1af014e
Артем Иванов <myotraykt@yandex.ru>
2024-09-30 17:09:12 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import parseJsonFile from "../src/parser.js";
 
 const program = new Command();
 
@@ -7,10 +8,14 @@ program
   .name('gendiff')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('')
+  .version('1.0.0')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(`Comparing ${filepath1} and ${filepath2}`);
+    const data1 = parseJsonFile(filepath1);
+    const data2 = parseJsonFile(filepath2);
+
+    console.log('Data from file 1:', data1);
+    console.log('Data from file 2', data2);
   });
 
 program.on('--help', () => {
